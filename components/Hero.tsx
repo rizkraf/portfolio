38af6f7eb886095851eb1dd54377a35eeb38ad6f
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2025-12-01 16:44:27 +0700

    fix: enhance hero section layout in desktop mode

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,14 +5,14 @@ import { Reveal } from "@/components/Reveal";
 
 export default function Hero() {
   return (
-    <section className="w-full max-w-7xl mx-auto flex flex-col justify-center lg:min-h-screen px-6 pt-32 lg:pt-20">
+    <section className="w-full max-w-7xl mx-auto flex flex-col justify-center lg:min-h-screen px-6 pt-32 lg:pt-0">
       <Reveal>
         <h1 className="text-[13vw] md:text-[10vw] leading-[0.85] md:leading-[0.8] font-bold tracking-tighter mix-blend-difference text-left">
           RIZKY RAFI <br />
           <span className="text-accent">AZHARA</span>
         </h1>
       </Reveal>
-      <div className="mt-12 flex flex-col md:flex-row justify-between items-start md:items-end gap-8">
+      <div className="mt-12 flex flex-col justify-between items-start gap-8">
         <Reveal delay={0.4}>
           <p className="text-xl md:text-2xl max-w-xl font-medium leading-relaxed">
             I craft seamless digital solutions, blending <span className="text-accent">front-end elegance</span> with <span className="text-accent">back-end robustness</span>.
